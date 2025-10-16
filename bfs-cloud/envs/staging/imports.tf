9053506c30eb617d56572d6260093595953c92a4
Levyn Schneider <lyschneider@icloud.com>
2025-10-16 22:53:56 +0200

    Remove terraform imports

--- bfs-cloud/envs/staging/imports.tf
+++
@@ -1,5 +0,0 @@
-import {
-  to = module.bfs_infrastructure.module.acr.azurerm_container_registry.this
-
-  id = "/subscriptions/5c3f67c0-445b-4317-a959-e9d5c663dc16/resourceGroups/bfs-staging-rg/providers/Microsoft.ContainerRegistry/registries/bfsstagingacr"
-}
