55ba5289cade9407d8e81b81af8cbafddf0a7cf4
Levyn Schneider <lyschneider@icloud.com>
2025-10-16 22:14:48 +0200

    Add health endpoint and remove imports

--- bfs-cloud/envs/staging/imports.tf
+++
@@ -1,9 +0,0 @@
-import {
-  id = "/subscriptions/5c3f67c0-445b-4317-a959-e9d5c663dc16/resourceGroups/bfs-staging-rg/providers/Microsoft.App/containerApps/backend-staging"
-  to = module.bfs_infrastructure.module.apps["backend-staging"].azurerm_container_app.this
-}
-
-import {
-  id = "/subscriptions/5c3f67c0-445b-4317-a959-e9d5c663dc16/resourceGroups/bfs-staging-rg/providers/Microsoft.App/containerApps/frontend-staging"
-  to = module.bfs_infrastructure.module.apps["frontend-staging"].azurerm_container_app.this
-}
