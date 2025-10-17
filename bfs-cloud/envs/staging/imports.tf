bb237514af9128a969a5c5f513e0821e88c9a95f
Levyn Schneider <lyschneider@icloud.com>
2025-10-17 12:03:28 +0200

    Fix imports

--- bfs-cloud/envs/staging/imports.tf
+++ bfs-cloud/envs/staging/imports.tf
@@ -1,9 +1,9 @@
 import {
-  to = module.bfs_infrastructure.apps["frontend-staging"].azurerm_container_app.this
+  to = module.bfs_infrastructure.module.apps["frontend-staging"].azurerm_container_app.this
   id = "/subscriptions/5c3f67c0-445b-4317-a959-e9d5c663dc16/resourceGroups/bfs-staging-rg/providers/Microsoft.App/containerApps/frontend-staging"
 }
 
 import {
-  to = module.bfs_infrastructure.apps["backend-staging"].azurerm_container_app.this
+  to = module.bfs_infrastructure.module.apps["backend-staging"].azurerm_container_app.this
   id = "/subscriptions/5c3f67c0-445b-4317-a959-e9d5c663dc16/resourceGroups/bfs-staging-rg/providers/Microsoft.App/containerApps/backend-staging"
 }
