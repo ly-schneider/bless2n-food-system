87f4ceba7cbbacccd87d766af8ab7a78abe65c97
Levyn Schneider <lyschneider@icloud.com>
2025-10-21 19:09:25 +0200

    Remove console log

--- bfs-web-app/app/api/auth/otp/verify/route.ts
+++ bfs-web-app/app/api/auth/otp/verify/route.ts
@@ -24,9 +24,6 @@ export async function POST(req: Request) {
     body: JSON.stringify(body),
   })
 
-  console.log("OTP verify request body:", body)
-  console.log("OTP verify response status:", res.status)
-
   if (!res.ok) {
     return NextResponse.json({ error: true, message: "Invalid code" }, { status: 401 })
   }
