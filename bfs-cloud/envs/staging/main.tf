2de0cc9deacfcf0c7adfea7160306a7b58585afd
Levyn Schneider <lyschneider@icloud.com>
2025-11-29 21:34:57 +0100

    Remove import

--- bfs-cloud/envs/staging/main.tf
+++ bfs-cloud/envs/staging/main.tf
@@ -1,8 +1,3 @@
-import {
-  to = module.bfs_infrastructure.module.cosmos.azurerm_cosmosdb_account.this
-  id = "/subscriptions/a6e97570-b80b-4a4d-a27c-081fe6c7e7f3/resourceGroups/bfs-staging-rg/providers/Microsoft.DocumentDB/databaseAccounts/bfs-staging-cosmos"
-}
-
 locals {
   registry_host = "ghcr.io"
   repo_prefix   = "ly-schneider/bless2n-food-system"
