a883c69640ebb757f978a63f98aba95aef4b9d00
Levyn Schneider <lyschneider@icloud.com>
2025-12-10 21:25:51 +0100

    Revert "[skip ci] add import" [skip ci] Remove import
    This reverts commit 737a84473cc29c4b1b902e6debbab38b5bd95442.

--- bfs-cloud/envs/staging/import.tf
+++
@@ -1,5 +0,0 @@
-// Import existing diagnostic setting for backend-staging so Terraform manages it.
-import {
-  to = module.bfs_infrastructure.module.apps_backend["backend-staging"].module.diag.azurerm_monitor_diagnostic_setting.this
-  id = "/subscriptions/a6e97570-b80b-4a4d-a27c-081fe6c7e7f3/resourceGroups/bfs-staging-rg/providers/Microsoft.App/containerApps/backend-staging|backend-staging-diag"
-}
