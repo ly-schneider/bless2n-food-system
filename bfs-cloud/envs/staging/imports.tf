6c782ca78963224c4745d7e54601e1faea23880a
Levyn Schneider <lyschneider@icloud.com>
2026-01-10 14:55:13 +0100

    Remove import

--- bfs-cloud/envs/staging/imports.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = module.bfs_infrastructure.module.security.azurerm_key_vault.basic[0]
-  id = "/subscriptions/a6e97570-b80b-4a4d-a27c-081fe6c7e7f3/resourceGroups/bfs-staging-rg/providers/Microsoft.KeyVault/vaults/bfs-staging-keyvault"
-}
